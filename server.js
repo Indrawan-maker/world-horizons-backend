203e2ef8960519882f4546c9f2bf4de6289d2007
Indrawan <indrawanismail0@gmail.com>
2025-07-28 20:55:03 +0800

    write some data

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-import http from "http"
+import http from "node:http"
 
 const PORT = 8000
 
 const server = http.createServer((req, res) => {
-    res.end("this from server i create")
+    res.write("This is a form data\n")
+    res.write("This is a Auth data\n")
+    res.write("Hello world\n")
+    res.end("this from server i create" ,"utf8", () => console.log("response end"))
 })
 
 server.listen(PORT, console.log(`server running on port ${PORT}`))
