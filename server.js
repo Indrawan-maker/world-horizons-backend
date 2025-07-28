fdaf64692fc5b8c402537f969a407ecc5c4246a8
Indrawan <indrawanismail0@gmail.com>
2025-07-28 20:39:10 +0800

    sending data from server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("we now have a package json!!!!")
+import http from "http"
+
+const PORT = 8000
+
+const server = http.createServer((req, res) => {
+    res.end("this from server i create")
+})
+
+server.listen(PORT, console.log(`server running on port ${PORT}`))
