6c0f03c4335ee753ea262d0a533a079d22efac6f
Indrawan <indrawanismail0@gmail.com>
2025-07-29 22:27:33 +0800

    no need write

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ import http from "node:http"
 const PORT = 8000
 
 const server = http.createServer((req, res) => {
-    res.write("This is a form data\n")
-    res.write("This is a Auth data\n")
     res.write("Hello world\n")
     res.end("this from server i create" ,"utf8", () => console.log("response end"))
 })
