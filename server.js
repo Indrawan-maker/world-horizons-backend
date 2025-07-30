821ade0e92e2effe4bf53058c0b7e4a0c053823d
Indrawan <indrawanismail0@gmail.com>
2025-07-30 16:01:16 +0800

    response are that right or wrong url

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ import http from "node:http"
 const PORT = 8000
 
 const server = http.createServer((req, res) => {
-    res.write("Hello world\n")
-    res.end("this from server i create" ,"utf8", () => console.log("response end"))
+    if(req.url === '/api') {
+        res.end("I'm an api nano da") 
+        return
+    } 
+    res.end("this is a wrong url")
 })
 
 server.listen(PORT, console.log(`server running on port ${PORT}`))
