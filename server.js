41ae1a3cdd1735131259fdf716698d95de98e5d9
Indrawan <indrawanismail0@gmail.com>
2025-07-31 20:58:21 +0800

    add content type

--- server.js
+++ server.js
@@ -4,18 +4,12 @@ import { getDataFromDB } from "./database/db.js"
 const PORT = 8000
 
 
-/*
-Challenge:
-    1. Store our data in a const ‘destinations’.
-    2. When a GET request is received to the ‘/api' endpoint, send our JSON stringified data.
-    Think: What changes will you need to make to get this to work?
-*/
-
     
     const server = http.createServer( async (req, res) => {
         if (req.url === '/api' && req.method === 'GET') {
             try{
                 const destination = JSON.stringify(await getDataFromDB())
+                res.setHeader("Content-Type", "application/json")
                 res.end(destination)
                 console.log(destination)
             }
