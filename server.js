d5dc6c1e5c9602a1d71612cddefc847147d7a70b
Indrawan <indrawanismail0@gmail.com>
2025-07-31 14:22:35 +0800

    get data using async

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 import http from "node:http"
+import { getDataFromDB } from "./database/db.js"
 
 const PORT = 8000
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/api') {
-        res.end("I'm an api nano da") 
-        return
-    } 
-    res.end("this is a wrong url")
-})
+
+/*
+Challenge:
+    1. Store our data in a const ‘destinations’.
+    2. When a GET request is received to the ‘/api' endpoint, send our JSON stringified data.
+    Think: What changes will you need to make to get this to work?
+*/
+
+    
+    const server = http.createServer( async (req, res) => {
+        if (req.url === '/api' && req.method === 'GET') {
+            try{
+                const destination = JSON.stringify(await getDataFromDB())
+                res.end(destination)
+                console.log(destination)
+            }
+            catch (err) {
+                console.log(err)
+            }
+        }
+    })
 
 server.listen(PORT, console.log(`server running on port ${PORT}`))
