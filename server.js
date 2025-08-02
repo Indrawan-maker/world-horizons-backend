2b7daa800e23c5c39d57c69217e2b38f17a6f8e3
Indrawan <indrawanismail0@gmail.com>
2025-08-02 21:16:36 +0800

    handle different route when accessing the API

--- server.js
+++ server.js
@@ -4,15 +4,27 @@ import { getDataFromDB } from "./database/db.js"
 const PORT = 8000
 
 
-    
+
     const server = http.createServer( async (req, res) => {
+        const destination = await getDataFromDB()
+
         if (req.url === '/api' && req.method === 'GET') {
-                const destination = JSON.stringify(await getDataFromDB())
                 res.setHeader("Content-Type", "application/json")
                 res.statusCode = 200
                 res.end(destination)
-                console.log(destination)
-        } else {
+        } else if(req.url.startsWith('/api/continent') && req.method === 'GET') {
+            const continent = req.url.split("/").pop()
+
+            const filteredData = destination.filter((datas) => {
+                return datas.continent.toLowerCase() === continent.toLowerCase()
+            })
+
+            console.log(JSON.stringify(filteredData))
+            res.setHeader("Content-Type", "application/json")
+            res.statusCode = 200
+            res.end(JSON.stringify(filteredData))
+
+        }else {
             res.setHeader("Content-Type", "application/json")
             res.statusCode = 404
             res.end(JSON.stringify({error: "not found", message: "The requested route does not exist"}))
